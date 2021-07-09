ddd0585a95c6a62674273229778fdf8cdaf1a9f3
per.olofsson@assaabloy.com <per.e.olofsson@gmail.com>
2021-07-09 17:54:56 +0200

    No func

--- main.js
+++ main.js
@@ -2,31 +2,24 @@ miro.onReady(() => {
     miro.initialize({
         extensionPoints: {
             bottomBar: {
-                title: 'Sticker to shapes',
+                title: 'JIRA Paste',
                 svgIcon:
                     '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
                 positionPriority: 1,
                 onClick: async () => {
-                    // Get selected widgets
-                    let selectedWidgets = await miro.board.selection.get()
-
-                    // Filter stickers from selected widgets
-                    let stickers = selectedWidgets.filter((widget) => widget.type === 'STICKER')
-
-                    // Delete selected stickers
-                    await miro.board.widgets.deleteById(stickers.map((sticker) => sticker.id))
-
-                    // Create shapes from selected stickers
-                    await miro.board.widgets.create(
-                        stickers.map((sticker) => ({
-                            type: 'shape',
-                            text: sticker.text,
-                            x: sticker.x,
-                            y: sticker.y,
-                            width: sticker.bounds.width,
-                            height: sticker.bounds.height,
-                        })),
-                    )
+                    navigator.clipboard.readText()
+                        .then(text => {
+                            await miro.board.widgets.create(
+                                stickers.map((sticker) => ({
+                                    type: 'shape',
+                                    text: text,
+                                })),
+                            )
+                        })
+                        .catch(err => {
+                            // maybe user didn't grant access to read from clipboard
+                            console.log('Something went wrong', err);
+                        });
 
                     // Show success message
                     miro.showNotification('Stickers has been converted')
